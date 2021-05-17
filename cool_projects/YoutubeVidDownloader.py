ff5b9b9959d4428fccda1ae6b03666ebedb98feb
Heet Shah <heet1356@gmail.com>
2021-05-16 22:46:32 -0400

    New file.

--- cool_projects/YoutubeVidDownloader.py
+++ cool_projects/YoutubeVidDownloader.py
@@ -39,34 +39,3 @@ Button(root, text='DOWNLOAD', font='arial 15 bold', bg='black', fg='red', padx=1
                                                                                                             y=150)
 root.mainloop()
 
-"""THIS IS CONSOLE VERSION"""
-# link_input = input("Enter the youtube link: ")
-# video = YouTube(link_input)  # put link in the Youtube class
-# video_min = video.length / 60
-# video_hour = video.length / 3600
-# video_sec = video.length
-# ys = video.streams.get_highest_resolution()
-#
-# # Title of the video
-# print("Title: ", video.title)
-#
-# # Number of views of video
-# print("Number of views: ", video.views)
-#
-# # Length of video
-# if video_sec > 3600:
-#     print("Length of video: ", video_hour, "hours")
-# elif 3600 > video_sec > 60:
-#     print("Length of video: ", video_min, "minutes")
-# else:
-#     print("Length of video: ", video_sec, "seconds")
-#
-# # Rating of video
-# print("Rating: ", video.rating)
-#
-# print('')
-#
-# print("Downloading...")
-# time.sleep(1)
-# ys.download('C:/Users/heet1/Downloads')  # type directory name in parenthesis
-# print('Complete! File saved in [ Downloads ]')
