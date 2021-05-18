f200d28165d967de73faf100a623c012bb35404f
Heet Shah <heet1356@gmail.com>
2021-05-18 12:57:17 -0400

    New file

--- cool_projects/fibbonaci_sequence.py
+++ cool_projects/fibbonaci_sequence.py
@@ -1,5 +1,5 @@
 # determine whether a list of numbers follows the fibonacci sequence
-# fibbonaci sequence --> [0,1,1,2,3,5,8,13] --> add up the previous two numbers to get the next number in list
+# Fibonacci sequence --> [0,1,1,2,3,5,8,13] --> add up the previous two numbers to get the next number in list
 
 def determine_fib(list):
     count = 0
@@ -7,7 +7,8 @@ def determine_fib(list):
         if int(list[num]) == int(list[num - 2]) + int(list[num - 1]):
             count += 1
     if count == len(list) - 2:
-        return str(list) + ' is a fibinacci sequence'
-    return str(list) + ' is not a fibinacci sequence'
+        return str(list) + ' is a Fibonacci sequence'
+    return str(list) + ' is not a Fibonacci sequence'
 
-print(determine_fib([0,2,4,6,7]))
+
+print(determine_fib([0, 2, 4, 6, 7]))
