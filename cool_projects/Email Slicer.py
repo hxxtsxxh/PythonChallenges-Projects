1f0c6071742cd9972bfe1230746d239b27bd7bb9
Heet Shah <heet1356@gmail.com>
2022-03-27 20:12:40 -0400

    updated

--- cool_projects/Email Slicer.py
+++ cool_projects/Email Slicer.py
@@ -2,11 +2,10 @@
 Email Slicer:
 
 This is one of the convenient python projects that has a lot of use in the future. The program
-helps get you the username and domain name from an email address. You can even customize the application and send a 
+helps get you the username and domain name from an email address. You can even customize the application and send a
 message to the host with this information.
 
 """
-from colorama import Fore
 emails = []
 on = True
 
@@ -16,17 +15,17 @@ def process_email(email_address):
         new_email = email_address.split('@')
         username = new_email[0]
         domain = new_email[1]
-        emails.append(Fore.RED + 'Username: ' + username + '   Domain: ' + domain)
+        emails.append('Username: ' + username + '   Domain: ' + domain)
     elif enter_email == 's':
         print('')
         print(*emails, sep="\n")
-        print(Fore.WHITE + '')
+        print('')
     elif enter_email == 'q':
         print('')
-        print(Fore.GREEN + 'Goodbye!')
+        print('Goodbye!')
         quit()
     else:
-        print(Fore.YELLOW + 'Invalid Input')
+        print('Invalid Input')
         print('')
 
 
